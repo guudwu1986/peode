72457a057cb97d4ac1c92b84fbee240b199c476e
GuuD WU <guuuuud@gmail.com>
2014-06-25 15:19:58 -0400

    Create "linear_inverse_eigen_mod.f90"
    A module to solve a linear ODE inverse problem
    by eigenvalue-updating scheme.

---
+++ solver/linear_inverse/linear_inverse_eigen_mod.f90
@@ -0,0 +1,13 @@
+! A module to solve a linear-ODE inverse problem
+! via eigenvalue-updating scheme.
+
+! Used by Module Linear_Ode_Inverse_mod
+
+module Linear_Ode_Inverse_Eigen_mod
+
+  implicit none
+  private
+
+  contains
+
+end module Linear_Ode_Inverse_Eigen_mod
