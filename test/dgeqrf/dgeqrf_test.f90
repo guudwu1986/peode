d40b8e9c5dd7264f554245e3a92479b536c56fef
GuuD WU <guuuuud@gmail.com>
2014-06-29 20:44:33 -0400

    Modify "dgeqrf_test.f90"
    Check both Q and R,
    by multiplying generators of Q to R,
    to reconstruct A.

--- test/dgeqrf/dgeqrf_test.f90
+++ test/dgeqrf/dgeqrf_test.f90
@@ -2,7 +2,7 @@
 
 ! Generate matrix A randomly,
 ! do decomposition,
-! then compare A^T A - R^T R.
+! then compare A with QR.
 
 program main
 
@@ -15,11 +15,21 @@ program main
 
   double precision , dimension(m*n) , target &
     :: a
+  double precision , dimension(m*n) , target &
+    :: o
+  double precision , dimension(m*n) , target &
+    :: r
+  double precision , dimension(m) &
+    :: v
   double precision , dimension(m,m) &
-    :: ata
+    :: temp
 
   double precision , dimension(:,:) , pointer &
-    :: p
+    :: p_a
+  double precision , dimension(:,:) , pointer &
+    :: p_r
+  double precision , dimension(:,:) , pointer &
+    :: p_o
 
   double precision , dimension(min(m,n)) &
     :: tau
@@ -36,14 +46,11 @@ program main
     :: index2
 
   call random_number(a)
+  o = a
 
-  p ( 1:m , 1:n ) => a
-
-  ata = matmul ( transpose(p) , p )
-
-  if ( allocated(work) ) then
-    stop
-  end if
+  p_a ( 1:m , 1:n ) => a
+  p_r ( 1:m , 1:n ) => r
+  p_o ( 1:m , 1:n ) => o
 
   allocate ( work(1) )
   lwork = -1
@@ -56,14 +63,22 @@ program main
 
   write(*,*) info
 
-  do index1 = 2 , m
-    do index2 = 1 , min(index1-1,n)
-      p ( index1 , index2 ) = 0
+  r = 0
+  do index1 = 1 , m
+    do index2 = index1 , n
+      p_r ( index1 , index2 ) = p_a ( index1 , index2 )
     end do
   end do
 
-  ata = ata - matmul ( transpose(p) , p )
-  write(*,*) maxval(abs(ata))
+  do index1 = min(m,n) , 1 , -1
+    v = 0
+    v ( index1 ) = 1
+    v ( (index1+1) : m ) = p_a ( (index1+1) : m , index1 )
+    temp = matmul ( reshape(v,(/m,1/)) , reshape(v,(/1,m/)) )
+    p_r = p_r - tau(index1) * matmul ( temp , p_r )
+  end do
+
+  write(*,*) maxval ( abs ( p_r - p_o ) )
 
   stop
 
